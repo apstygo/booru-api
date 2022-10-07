4717fea38f386211bc39b70a39671860073d4ce6
Artem Pstygo <apstygo.dev@gmail.com>
2022-10-07 22:21:33 +0200

    setup base structure

--- Package.swift
+++ Package.swift
@@ -5,22 +5,10 @@ import PackageDescription
 let package = Package(
     name: "booru-api",
     products: [
-        .library(
-            name: "booru-api",
-            targets: ["booru-api"]),
+        .library(name: "SankakuAPI", targets: ["SankakuAPI"])
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
+    dependencies: [],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "booru-api",
-            dependencies: []),
-        .testTarget(
-            name: "booru-apiTests",
-            dependencies: ["booru-api"]),
+        .target(name: "SankakuAPI")
     ]
 )
